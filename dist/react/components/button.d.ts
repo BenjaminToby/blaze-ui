7cf40c19511246a609af8c693104f046f5f63f25
Benjamin Toby <benoti.san@gmail.com>
2024-09-07 17:55:57 +0100

    Remove Unnecessary files

--- dist/react/components/button.d.ts
+++
@@ -1,7 +0,0 @@
-import React, { AnchorHTMLAttributes, ButtonHTMLAttributes, DetailedHTMLProps } from "react";
-type Props = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> & DetailedHTMLProps<AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement> & {
-    variant?: "solid" | "outlined" | "ghost";
-    color?: "default" | "secondary" | "accent" | "gray" | "light";
-};
-export default function (props: Props): React.JSX.Element;
-export {};
