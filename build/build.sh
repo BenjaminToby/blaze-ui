8704a27049942ca1c7a789b55bb354bf94441023
Benjamin Toby <benoti.san@gmail.com>
2024-09-07 17:17:54 +0100

    Build Update

--- build/build.sh
+++ build/build.sh
@@ -2,24 +2,28 @@
 
 cd /media/benoti/Ubuntu-Disk/Github/ui-library/build
 
+echo "Building ..."
+
 bun index.ts
 
-if [ $? -eq 0 ]; then
-    cd /media/benoti/Ubuntu-Disk/Github/ui-library
-
-    rm -f /media/benoti/Ubuntu-Disk/Github/ui-library/dist/react/components/*.ts
-
-    cp -R /media/benoti/Ubuntu-Disk/Github/ui-library/types/d.ts/components/react/* \
-        /media/benoti/Ubuntu-Disk/Github/ui-library/dist/react/components/
-
-    git add .
-    git commit -m "Build Update"
-    git push
-    git checkout latest
-    git merge main
-    git push
-    git checkout main
+if [ $? -ne 0 ]; then
+    echo "Build Failed!"
+    exit 1
 else
-    echo FAIL
+    echo "Build Succeded!"
 fi
 
+cd /media/benoti/Ubuntu-Disk/Github/ui-library
+
+rm -f /media/benoti/Ubuntu-Disk/Github/ui-library/dist/react/components/*.ts
+
+cp -R /media/benoti/Ubuntu-Disk/Github/ui-library/types/d.ts/components/react/* \
+    /media/benoti/Ubuntu-Disk/Github/ui-library/dist/react/components/
+
+git add .
+git commit -m "Build Update"
+git push
+git checkout latest
+git merge main
+git push
+git checkout main
